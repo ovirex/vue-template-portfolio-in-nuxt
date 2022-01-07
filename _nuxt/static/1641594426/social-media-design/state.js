b6391e809906b91121aaa6c06dade8bc151fc97a
ovirex <ovirex@users.noreply.github.com>
2022-01-07 22:28:06 +0000

    deploy: 6616b9095e880f95edfd7492e58ee96159bf6dcf

--- _nuxt/static/1641594426/social-media-design/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641594426",layout:"CustomLayout",error:a,serverRendered:true,routePath:"\u002Fsocial-media-design",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d7602d6f"}}}}(null));
---
+++ _nuxt/static/1641594483/social-media-design/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641594483",layout:"CustomLayout",error:a,serverRendered:true,routePath:"\u002Fsocial-media-design",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"53cf1079"}}}}(null));
