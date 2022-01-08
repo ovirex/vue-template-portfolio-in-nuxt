ca5fffe410fd7064c06a7d14399dc4a651434f33
ovirex <ovirex@users.noreply.github.com>
2022-01-08 00:52:39 +0000

    deploy: 9e16278cf965dc9e9721cbd75b84dcdf7f80cae3

--- _nuxt/static/1641596977/web-design/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641596977",layout:"CustomLayout",error:a,serverRendered:true,routePath:"\u002Fweb-design",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ab50ef39"}}}}(null));
