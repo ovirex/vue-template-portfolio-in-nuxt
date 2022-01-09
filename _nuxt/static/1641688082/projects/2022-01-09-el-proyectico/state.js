2160ce579d13ccdba656623a4e1ba7a0e2c2d006
ovirex <ovirex@users.noreply.github.com>
2022-01-09 00:28:04 +0000

    deploy: 74acc577ef146d31757b669b098ea6497fed5bf6

---
+++ _nuxt/static/1641688082/projects/2022-01-09-el-proyectico/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641688082",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"11820487"}}}}("\u002Fprojects\u002F2022-01-09-el-proyectico"));
